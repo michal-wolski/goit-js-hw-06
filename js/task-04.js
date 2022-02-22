62feecb70eb9f68a467d47707069b190583e6e84
michal-wolski <michal.wolski130@gmail.com>
2022-02-22 15:00:59 +0100

    tast 4 done

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,20 @@
 "use strict"
+let $counterValue = document.querySelector("#value");
+const $btnDecrement = document.querySelector('button[data-action="decrement"]')
+const $btnIncrement = document.querySelector('button[data-action="increment"]');
+
+
+let counterValue = 0;
+
+const increment = () => {
+    counterValue += 1;
+    $counterValue.textContent = counterValue;
+  }
+  const decrement = () => {
+    counterValue -= 1;
+    $counterValue.textContent = counterValue;
+  };
+
+
+  $btnDecrement.addEventListener("click", decrement);
+  $btnIncrement.addEventListener("click", increment);
