4bbed4ed4247ad74e2824a9622751935ff65dbba
michal-wolski <michal.wolski130@gmail.com>
2022-02-23 19:04:19 +0100

    task 5 done

--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,10 @@
 "use strict"
+const $input = document.querySelector("#name-input");
+const $output = document.querySelector("#name-output");
+
+$input.addEventListener("input", (event) => {
+  if (event.target.value) {
+    $output.textContent = event.target.value;
+    $output.textContent = input
+  } else { $output.textContent = `Anonymous`};
+});
