bf612460fa15946460dfa82463e0ffe42b6b3100
michal-wolski <michal.wolski130@gmail.com>
2022-02-24 12:18:53 +0100

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,2 +1,23 @@
 "use strict"
 
+// Napisz skrypt zarządzania formularzem logowania.
+// Opracowanie przesyłania formularza form.login-form powinno przebiegać zgodnie ze zdarzeniem submit.
+// Podczas przesyłania formularza strona nie powinna się restartować.
+// Jeśli w formularzu są nieuzupełnione pola, wprowadź alert z upomnieniem o tym, że wszystkie pola powinny zostać wypełnione.
+// Jeśli użytkownik uzupełnił wszystkie pola i wysłał formularz, zbierz wartość pól jako obiekt, gdzie nazwa pola będzie nazwą właściwości, a wartość pola - wartością właściwości. Aby otrzymać dostęp do elementów formularza użyj właściwości elements.
+// Umieść obiekt z wprowadzonymi danymi do wiersza poleceń i wyczyść wartości pól formularza metodą reset.
+
+
+const $form = document.querySelector(".login-form")
+
+$form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const elements = {email: $form.email.value, password:$form.password.value}
+    const validation = elements.email && elements.password;
+    if (validation == false) {
+        alert("Proszę uzupełnić wszystkie pola")
+    }
+    console.log(elements);
+    
+})
+
